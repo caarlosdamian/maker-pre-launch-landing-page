d5fc7a1b9c4b6b0e9329b4f503bc8f8eb7bbd62e
caarlosdamian <dapecarlos@gmail.com>
2022-08-16 20:23:23 -0500

    feat: added infoCards

---
+++ src/utils/index.ts
@@ -0,0 +1,26 @@
+export const infoCards = [
+  {
+    id: 1,
+    title: 'Indulge your passions',
+    description:
+      "Your passions shouldn't be just for the weekend. Earn a living doing what you love.",
+  },
+  {
+    id: 2,
+    title: 'Gain financial freedom',
+    description:
+      'Start making money work for you. There’s nothing quite like earning while you sleep.',
+  },
+  {
+    id: 3,
+    title: 'Choose your lifestyle',
+    description:
+      'Own your daily schedule. Fancy a lie-in? Go for it! Take charge of your week.',
+  },
+  {
+    id: 4,
+    title: 'Work from anywhere',
+    description:
+      'Selling online means not being pinned down. Want to work AND travel? Go for it!',
+  },
+];
